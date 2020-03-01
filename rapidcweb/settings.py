8bc8f111a43aa0cac10d200d5f4138a380c66f28
William Gunnells <gunnells@gmail.com>
2020-03-01 15:30:38 -0800

    collect css

--- rapidcweb/settings.py
+++ rapidcweb/settings.py
@@ -15,8 +15,8 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-# TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
-TEMPLATE_DIR = os.path.join(BASE_DIR, 'staticfiles')
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+#TEMPLATE_DIR = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 LOGIN_REDIRECT_URL = '/'
